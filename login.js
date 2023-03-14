3400f063b22d2d5e216626a20a8d36a9fcbdc020
abhishek <abhishekraibagi81@gmail.com>
2023-03-14 17:30:00 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,28 @@
+const loginForm = document.getElementById("login-form");
+
+const loginButton = document.getElementById("login-form-submit");
+
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const username = loginForm.username.value;
+
+    const password = loginForm.password.value;
+
+    if (username === "admin" && password === "admin143") {
+
+        alert("You have successfully logged in.");
+
+        window.location.href="/main.jsp";
+
+    } 
+
+    else {
+
+        loginErrorMsg.style.opacity = 1;
+    }
+}
+)
